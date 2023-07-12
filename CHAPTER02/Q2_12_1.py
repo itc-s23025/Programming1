70fcfa830c028f981dfd627790b3ad6de1ae8991
miyagi_yuuki <s23025@std.it-college.ac.jp>
2023-07-12 00:52:32 +0000

    add

---
+++ CHAPTER02/Q2_12_1.py
@@ -0,0 +1,4 @@
+import math
+
+print(math.pow(2, 3))
+# mathモジュールをimportして組み込み型関数pow()を使用して冪乗を実行します。
---
+++ CHAPTER02/Q2_12_2.py
@@ -0,0 +1,4 @@
+print(random.randint(0, 10))
+# このコードはエラーになり、実行できませんが。randomモジュールをimportして実行すれば使用することができます。
+# 例 import random
+# print(random.randint(0, 10))
