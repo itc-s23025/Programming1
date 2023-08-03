aa95cf71ded24e183029d64c686befda6a445d7d
miyagi_yuuki <s23025@std.it-college.ac.jp>
2023-08-03 05:19:50 +0000

    add

---
+++ CHAPTER04/Q4_4_1.py
@@ -0,0 +1,30 @@
+vote_num = 0
+
+
+def vote():
+    print("投票します")
+    global vote_num
+    vote_num += 1
+
+
+def reset_box():
+    global vote_num
+    print("箱を空にします")
+    vote_num = 0
+
+
+def check_box():
+    global vote_num
+    print("栗の数は {} です".format(vote_num))
+
+
+# 実行例
+vote()
+check_box()
+vote()
+check_box()
+for i in range(3):
+    vote()
+check_box()
+reset_box()
+check_box()
