37d027f9a29655754d02fa033a8537f1c33f0a3e
miyagi_yuuki <s23025@std.it-college.ac.jp>
2023-08-15 07:00:15 +0000

    add

---
+++ CHAPTER05/Q5_7_1.py
@@ -0,0 +1,7 @@
+yamanote_line = {0: "Tokyo", 1: "Yuurakuchou", 2: "Shinbashi"}
+for v in yamanote_line.values():
+    print(v)
+
+# 辞書の値が出力されますが、辞書変数内の順序はハッシュ値によりますので、順不同となります。
+
+# このプログラムの正しい出力結果として(Tokyo, Yuurakuchou, Shinbashi)が出力されます。
